094f6ce434848b0ccee423af0746062e74de0d34
Antoine Drabble <drabble@MOB8700.hq.k.grp>
2014-07-30 16:40:19 +0200

    Add management buttons images

--- src/lightleaf/panel/HeaderPanel.java
+++ src/lightleaf/panel/HeaderPanel.java
@@ -4,13 +4,40 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
  
 public class HeaderPanel extends JPanel { 
 	
+	private Image exit;
+	private Image maximize;
+	private Image minimize;
+	
 	public HeaderPanel(){
 		this.setPreferredSize( new Dimension( this.getWidth(), 40) );
+		
+		try {
+	      maximize = ImageIO.read(new File("maximize.png"));
+	    } catch (IOException e) {
+	      e.printStackTrace();
+	    }
+		
+		try {
+	      minimize = ImageIO.read(new File("minimize.png"));
+	    } catch (IOException e) {
+	      e.printStackTrace();
+	    }
+		
+		try {
+	      exit = ImageIO.read(new File("exit.png"));
+	    } catch (IOException e) {
+	      e.printStackTrace();
+	    }
 	}
 	
 	public void paintComponent(Graphics g){
@@ -20,6 +47,13 @@ public class HeaderPanel extends JPanel {
 	    g.setFont(font);
 	    g.setColor(Color.white);          
 	    g.drawString("LightLeaf", 15, 25);   
+	    
+	    
+	    Graphics2D g2d = (Graphics2D)g;
+
+	    g2d.drawImage(exit, this.getWidth() - 15, 25, 32, 32, this);
+	    g2d.drawImage(maximize, this.getWidth() - 25, 25, 32, 32, this);
+	    g2d.drawImage(minimize, this.getWidth() - 35, 25, 32, 32, this);
 	}     
   
 }
